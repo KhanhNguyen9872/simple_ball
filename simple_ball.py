f5306f80ecb321aa5555e9da626023bede40faeb
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2023-09-22 22:40:57 +0700

    up

--- simple_ball.py
+++ simple_ball.py
@@ -9,13 +9,15 @@ color = ["\033[94m", "\033[96m", "\033[92m", "\033[93m", "\033[91m", "\033[95m",
 stdout = khanhnguyen9872.stdout()
 stdout_2 = khanhnguyen9872.stdout()
 
-wall_portrait = random.randint(4, 31)
-wall_landscape = random.randint(2, 13)
+string_ball = "●"
+string_bar = "▀▀▀▀▀"
+
+wall_portrait = random.randint(len(string_bar)+1, 31)
+wall_landscape = random.randint(3, 13)
 
 landscape = wall_landscape
 timeout = 0.06
 boolvar = False
-string_ball = "O"
 string_wall_portrait = color[-1] + "="
 string_wall_landspace = color[-1] + "|"
 
@@ -68,30 +70,59 @@ def __wall_landscape__():
     except KeyboardInterrupt:
         return
 
+def __random_place_bar__():
+    global tmp_place, is_exit
+    try:
+        while 1:
+            tmp_place = random.randint(2, len(string_bar))
+            time.sleep(2)
+    except KeyboardInterrupt:
+        return
+
 #### MAIN
 stdout_2.hide_cursor()
 stdout_2.write(color[-1])
 stdout_2.write("\n>> Hello World!\n>> Hey! What are you doing?\n\n[Ctrl + C] -> EXIT\n\n")
 
 try:
+    __string_bar__ = color[-1] + string_bar
+    tmp_place = 2
+    tmp_color = None
     threading.Thread(target=__auto_resize__).start()
+    threading.Thread(target=__random_place_bar__).start()
     while 1:
-        __string_wall_portrait__ = string_wall_portrait * (wall_portrait + len(string_ball) + 2)
-        __string_wall_landscape__ = string_wall_landspace + " " * (wall_portrait+len(string_ball)) + string_wall_landspace + "\n"
-        
+        __string_wall_portrait__ = " " + string_wall_portrait * (wall_portrait + len(string_ball) + 2)
+        __string_wall_landscape__ = " " + string_wall_landspace + " " * (wall_portrait+len(string_ball)) + string_wall_landspace + "\n"
+
         stdout.write(__string_wall_portrait__ + "\n")
+        tmp_color = random.choice(color)
+
+        # Ball
         stdout.write(
             __string_wall_landscape__ * landscape
             + ( 
-                string_wall_landspace
+                " " 
+                + string_wall_landspace
                 + " " * portrait
-                + random.choice(color) 
+                + tmp_color 
                 + string_ball
                 + " " * (wall_portrait - portrait)
                 + (string_wall_landspace if wall_portrait + 1 > portrait else f"{string_wall_landspace}<<==") 
                 + "\n"
             ) 
             + __string_wall_landscape__ * (wall_landscape - landscape)
+        )
+
+        # Bar
+        tmp_value_bar = int(len(string_bar) / tmp_place)
+        tmp_place_var = int(portrait - tmp_value_bar)
+        if tmp_place_var <= 0:
+            tmp_place_var = 1
+        stdout.write(
+                " " * tmp_place_var
+                + __string_bar__
+                + " " * (wall_portrait - portrait - tmp_value_bar)
+                + "\n"
         )
         stdout.write(__string_wall_portrait__)
 
@@ -101,6 +132,8 @@ try:
             landscape += 1
         else:
             if landscape == wall_landscape or landscape == 0:
+                if not boolvar:
+                    __string_bar__ = tmp_color + string_bar
                 boolvar = not boolvar
             if boolvar:
                 landscape -= 1
